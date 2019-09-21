33084a2b68b55fd775f83425cc054dd1d926e0ce
Thirukumaraned <Thirukumaran@medivet.co.ukdasf>
2019-09-21 19:31:36 +0530

    added temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            //console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This hight today is '+ body.daily.data[0].temperatureHigh +' There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
